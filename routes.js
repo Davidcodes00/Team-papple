574f1d2fed7ee950a199770ac34e649ae6d599ca
Ciara <cduncumnz@gmail.com>
2022-06-03 14:40:42 +1200

    updated final post route

--- routes.js
+++ routes.js
@@ -16,14 +16,14 @@ router.get('/:id', async (req, res) => {
     const animalData = await fspromises.readFile(filepath, 'utf8')
     //parse data from string back into object
     const parsedAnimalData = JSON.parse(animalData)
-    console.log('parsed data', parsedAnimalData)
+    //console.log('parsed data', parsedAnimalData)
     //get animals array out of the object in out data.json
     const arrayOfAnimals = parsedAnimalData.animals
     //now we have access to our animals array match to ID
     let theChosenAnimal = arrayOfAnimals.find(
       (element) => element.id === Number(req.params.id)
     )
-    console.log('the chosed animal', theChosenAnimal)
+    //console.log('the chosen animal', theChosenAnimal)
     return res.render('lastwords', theChosenAnimal)
   } catch (err) {
     console.log(err, 'I could not find your chosen animal')
@@ -31,20 +31,31 @@ router.get('/:id', async (req, res) => {
 })
 
 router.post('/:id', async (req, res) => {
+  console.log('in the post route')
   const id = req.params.id
   try {
     const userDetails = await fspromises.readFile(filepath, 'utf8')
     const parsedUserDetails = JSON.parse(userDetails)
+    console.log(parsedUserDetails)
 
     //get the data from the user array
     const userDetailsArray = parsedUserDetails.userDetails
-    console.log('the users details:', userDetailsArray)
-    userDetailsArray.username = req.body.username
-    userDetailsArray.lastwords = req.body.lastwords
-    let updatedUserDetails = userDetailsArray
+    let newArray = { ...userDetailsArray }
+    console.log('the users details:', newArray)
 
-    const stringData = JSON.stringify(updatedUserDetails, null, 2)
-    console.log('updated users details are:', updatedUserDetails)
+    newArray.lastwords = req.body.lastwords
+    //newArray.username = req.body.username
+    //console.log('new array. last words', newArray.lastwords)
+
+    let updatedUserDetails = newArray
+    console.log('updatedUser', updatedUserDetails)
+    let entireArray = {
+      animals: parsedUserDetails.animals,
+      userDetails: updatedUserDetails,
+    }
+    console.log('entire array: ', entireArray)
+
+    const stringData = JSON.stringify(entireArray, null, 2)
     await fspromises.writeFile(filepath, stringData, 'utf8')
     res.redirect('/:id/fight')
   } catch (err) {
