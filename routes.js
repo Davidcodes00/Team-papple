5a80e53e02c1fdfada923501cd92228aab534886
Erin <erin.lizzie.rose@gmail.com>
2022-06-03 12:48:10 +1200

    Added route to the last words page

--- routes.js
+++ routes.js
@@ -12,8 +12,20 @@ const filepath = path.join(__dirname, 'data.json')
 
 router.get('/:id', async (req, res) => {
   try {
-    const animalData = await fspromises.promises.readFile(filepath, 'utf8')
+    //read our data
+    const animalData = await fspromises.readFile(filepath, 'utf8')
+    //parse data from string back into object
     const parsedAnimalData = JSON.parse(animalData)
-    res.render('lastwords')
-  } catch (err) {}
+    console.log('parsed data', parsedAnimalData)
+    //get animals array out of the object in out data.json
+    const arrayOfAnimals = parsedAnimalData.animals
+    //now we have access to our animals array match to ID
+    let theChosenAnimal = arrayOfAnimals.find(
+      (element) => element.id === Number(req.params.id)
+    )
+    console.log('the chosed animal', theChosenAnimal)
+    return res.render('lastwords', theChosenAnimal)
+  } catch (err) {
+    console.log(err, 'I could not find your chosen animal')
+  }
 })
