3c2135a0b2611f0e01611116add518713022f6ed
Erin <erin.lizzie.rose@gmail.com>
2022-06-03 12:17:06 +1200

    added some routes

--- routes.js
+++ routes.js
@@ -8,3 +8,14 @@ const filepath = path.join(__dirname, 'data.json')
 
 //export router
 module.exports = router
+
+//filepath
+const filepath = path.join(__dirname, 'data.json')
+
+router.get('/:id', async (req, res) => {
+  try {
+    const animalData = await fspromises.promises.readFile(filepath, 'utf8')
+    const parsedAnimalData = JSON.parse(animalData)
+    res.render('lastwords')
+  } catch (err) {}
+})
